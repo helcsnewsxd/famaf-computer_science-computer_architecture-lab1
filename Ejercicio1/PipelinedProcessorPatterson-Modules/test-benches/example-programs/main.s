445945f02f0204ae54b37be140ee081af958d999
helcsnewsxd <helcsnewsxd@helcsnewsxd>
2023-10-14 17:28:12 -0300

    Mejorado process.sh

--- Ejercicio1/PipelinedProcessorPatterson-Modules/test-benches/example-programs/main.s
+++ Ejercicio1/PipelinedProcessorPatterson-Modules/test-benches/example-programs/main.s
@@ -1,78 +1,66 @@
     .text
     .org 0x0000
+    
+    // Dirección: valor
+    STUR X1, [X0, #0] // MEM 0:0x1
+    STUR X2, [X0, #8] // MEM 1:0x2
+    STUR X3, [X16, #0] // MEM 2:0x3
+    ADD X3, X4, X5
+        ADD XZR, XZR, XZR
+    STUR X3, [X0, #24] // MEM 3:0x9
+    SUB X3, X4, X5
+        ADD XZR, XZR, XZR
+    STUR X3, [X0, #32] // MEM 4:0xFFFFFFFFFFFFFFFF
+    SUB X4, XZR, X10
+        ADD XZR, XZR, XZR
+    STUR X4, [X0, #40] // MEM 5:0xFFFFFFFFFFFFFFF6
+    ADD X4, X3, X4
+        ADD XZR, XZR, XZR
+    STUR X4, [X0, #48] // MEM 6:0xFFFFFFFFFFFFFFF5
+    SUB X5, X1, X3
+        ADD XZR, XZR, XZR
+    STUR X5, [X0, #56] // MEM 7:0x2
+    AND X5, X10, XZR
+        ADD XZR, XZR, XZR
+    STUR X5, [X0, #64] // MEM 8:0x0
+    AND X5, X10, X3
+        ADD XZR, XZR, XZR
+    STUR X5, [X0, #72] // MEM 9:0xA
+    AND X20, X20, X20
+        ADD XZR, XZR, XZR
+    STUR X20, [X0, #80] // MEM 10:0x14
+    ORR X6, X11, XZR
+        ADD XZR, XZR, XZR
+    STUR X6, [X0, #88] // MEM 11:0xB
+    ORR X6, X11, X3
+        ADD XZR, XZR, XZR
+    STUR X6, [X0, #96] // MEM 12:0xFFFFFFFFFFFFFFFF
+    LDUR X12, [X0, #0]
+    ADD X7, X12, XZR
+        ADD XZR, XZR, XZR
+    STUR X7, [X0, #104] // MEM 13:0x1
+    STUR X12, [X0, #112] // MEM 14:0x1
+    ADD XZR, X13, X14
+    STUR XZR, [X0, #120] // MEM 15:0x0
+    CBZ X0, L1
+    STUR X21, [X0, #128] // MEM 16:0x0(si falla CBZ =21)
 
-    zero .req xzr
-    one .req x1
-    mvMem .req x8
+L1: STUR X21, [X0, #136] // MEM 17:0x15
+    ADD X2, XZR, X1
 
-    posMem .req x0
-    var .req x2
-    var2 .req x3
+L2: SUB X2, X2, X1
+    ADD X24, XZR, X1
+        ADD XZR, XZR, XZR
+    STUR X24, [X0, #144] // MEM 18:0x1 y MEM 19=0x1
+    ADD X0, X0, X8
+    CBZ X2, L2
+    STUR X30, [X0, #144] // MEM 20:0x1E
+    ADD X30, X30, X30
+    SUB X21, XZR, X21
+    ADD X30, X30, X20
+    LDUR X25, [X30, #-8]
+    ADD X30, X30, X30
+    ADD X30, X30, X16
+    STUR X25, [X30, #-8] // MEM 21:0xA (= MEM 9)
 
-    limitVal .req x30
-    it .req x29
-
-    fstReg .req x16
-    sndReg .req x17
-
-problem_2a:
-    add var, zero, zero
-    add posMem, zero, zero
-
-    add it, zero, limitVal
-    problem_2a_while:
-        cbz it, problem_2a_end_while
-
-        stur var, [posMem, #0]
-        add var, var, one
-        add posMem, posMem, mvMem
-
-        sub it, it, one
-        cbz zero, problem_2a_while
-    problem_2a_end_while:
-
-problem_2b:
-    add var, zero, zero
-    add var2, zero, zero
-    add posMem, zero, zero
-
-    add it, zero, limitVal
-    problem_2b_while:
-        cbz it, problem_2b_end_while
-
-        ldur var, [posMem, #0]
-        add var2, var2, var
-        add posMem, posMem, mvMem
-
-        sub it, it, one
-        cbz zero, problem_2b_while
-    problem_2b_end_while:
-
-    stur var2, [posMem, #0]
-
-problem_2c:
-    add var, zero, zero
-    add posMem, zero, zero
-
-    add it, zero, fstReg
-    problem_2c_while:
-        cbz it, problem_2c_end_while
-
-        add var, var, sndReg
-
-        sub it, it, one
-        cbz zero, problem_2c_while
-    problem_2c_end_while:
-
-    stur var, [posMem, #0]
-
-    .unreq zero
-    .unreq one
-    .unreq mvMem
-    .unreq posMem
-    .unreq var
-    .unreq var2
-    .unreq limitVal
-    .unreq it
-    .unreq fstReg
-    .unreq sndReg
+finloop: CBZ XZR, finloop
--- Ejercicio1/PipelinedProcessorPatterson-Modules/test-benches/example-programs/process.sh
+++ Ejercicio1/PipelinedProcessorPatterson-Modules/test-benches/example-programs/process.sh
@@ -1,12 +1,37 @@
-make 
+make
 
 echo
 
 LONG=$(cat main.list | grep -oP '\t[0-9a-f]{8}' | wc -l)
 
-#cat main.list | grep -oP '\t[0-9a-f]{8}' | awk -v awkvar="$LONG" '{print awkvar}'
+TOTAL=64
+CNT=1
 
-cat main.list | grep -oP '\t[0-9a-f]{8}' | awk -v awkvar="$LONG" '{if (NR==1) {print "ROM [0:" awkvar-1 "] =\x27{32\x27h" $1 ","} else { if (NR==awkvar){print "32\x27h" $1 "};"} else {print "32\x27h" $1 ","}}}'
+echo -n "logic [N-1 : 0] ROM[0 : 63] = '{"
+
+cat main.list | grep -oP '\t[0-9a-f]{8}' | while true; do
+    while read -r line; do
+        if [ $CNT -eq $TOTAL ]; then
+            echo -n "32'h$line"
+            CNT=$((CNT+1))
+        else
+            echo -n "32'h$line,"
+            CNT=$((CNT+1))
+        fi
+    done
+
+    for i in $(seq $CNT $TOTAL); do
+        if [ $i -eq $TOTAL ]; then
+            echo -n "32'h00000000"
+        else
+            echo -n "32'h00000000,"
+        fi
+    done
+
+    break
+done
+
+echo "};"
 
 echo
 
